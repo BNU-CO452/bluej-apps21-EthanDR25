b7b554dd662d9adf15588367fa47509d44be53ad
EthanDR52 <91742651+EthanDR25@users.noreply.github.com>
2021-12-03 03:08:41 +0000

    Add files via upload
    This is my work for app 4 - Ethan Dias-Richards

--- app214-stock/Program.java
+++ app214-stock/Program.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class Program here.
+ * This is the Program class which executes the necessary code required
+ * to run the StockApp.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Ethan Dias-Richards
+ * @version 2/12/2021
  */
 public class Program
 {
